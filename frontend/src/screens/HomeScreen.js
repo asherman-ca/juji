671c90fb27fd4e1197e9c19d6b702dcf58073bd5
Alex Sherman <asherman.ca@gmail.com>
2021-08-19 15:03:22 -0700

    spinner working

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from '../components/Loader'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <Loader />
+    </>
+  )
+}
+
+export default HomeScreen
